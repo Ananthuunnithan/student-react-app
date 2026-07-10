538f2b784c93261f5ea93cb17fdbf8176bf2ff0b
Ananthu Unnithan <unnithanananthu69@gmail.com>
2026-07-10 10:38:52 +0530

    Made dynamic

--- src/components/ViewStudent.jsx
+++ src/components/ViewStudent.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const ViewStudent = () => {
+
+    const [data, changeData] = useState(
+        [
+            { "name": "Rahul Sharma", "avatar": "https://randomuser.me/api/portraits/men/1.jpg", "course": " XII - A", "admn": "STU001"}, 
+            { "name": "Anjali Nair", "avatar": "https://randomuser.me/api/portraits/women/2.jpg", "course": " XI - B", "admn": "STU002"}, 
+            { "name": "Arun Kumar", "avatar": "https://randomuser.me/api/portraits/men/3.jpg", "course": "X - A", "admn": "STU003"}, 
+            { "name": "Meera Joseph", "avatar": "https://randomuser.me/api/portraits/women/4.jpg", "course": "IX - C", "admn": "STU004"}, 
+            { "name": "Vishnu Das", "avatar": "https://randomuser.me/api/portraits/men/5.jpg", "course": "VIII - A", "admn": "STU005"}, 
+            { "name": "Sneha Menon", "avatar": "https://randomuser.me/api/portraits/women/6.jpg", "course": "VII - B", "admn": "STU006"}
+        ]
+    )
+
     return (
         <div>
             <NavBar />
@@ -10,184 +22,28 @@ const ViewStudent = () => {
                 <div className="row g-4">
 
                     {/* Student 1 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+                    {data.map(
+                        (value,index) =>{
+                            return(
+                                <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                         <div className="card h-100">
                             <img
-                                src="https://randomuser.me/api/portraits/men/1.jpg"
+                                src={value.avatar}
                                 className="card-img-top"
                                 alt="Student"
                                 height="300px"
                             />
                             <div className="card-body text-center">
-                                <h5 className="card-title">Rahul Sharma</h5>
-                                <p><strong>Admission No:</strong> STU001</p>
-                                <p><strong>Class:</strong> XII - A</p>
+                                <h5 className="card-title">{value.name}</h5>
+                                <p><strong>{value.admn}</strong> STU001</p>
+                                <p><strong>{value.course}</strong> XII - A</p>
                                 <button className="btn btn-primary">View Details</button>
                             </div>
                         </div>
                     </div>
-
-                    {/* Student 2 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/women/2.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Anjali Nair</h5>
-                                <p><strong>Admission No:</strong> STU002</p>
-                                <p><strong>Class:</strong> XI - B</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Student 3 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/men/3.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Arun Kumar</h5>
-                                <p><strong>Admission No:</strong> STU003</p>
-                                <p><strong>Class:</strong> X - A</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Student 4 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/women/4.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Meera Joseph</h5>
-                                <p><strong>Admission No:</strong> STU004</p>
-                                <p><strong>Class:</strong> IX - C</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Student 5 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/men/5.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Vishnu Das</h5>
-                                <p><strong>Admission No:</strong> STU005</p>
-                                <p><strong>Class:</strong> VIII - A</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Student 6 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/women/6.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Sneha Menon</h5>
-                                <p><strong>Admission No:</strong> STU006</p>
-                                <p><strong>Class:</strong> VII - B</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Student 7 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/men/7.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Akash Roy</h5>
-                                <p><strong>Admission No:</strong> STU007</p>
-                                <p><strong>Class:</strong> VI - C</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Student 8 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/women/8.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Priya Thomas</h5>
-                                <p><strong>Admission No:</strong> STU008</p>
-                                <p><strong>Class:</strong> V - A</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Student 9 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/men/9.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Rohan Singh</h5>
-                                <p><strong>Admission No:</strong> STU009</p>
-                                <p><strong>Class:</strong> IV - B</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Student 10 */}
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100">
-                            <img
-                                src="https://randomuser.me/api/portraits/women/10.jpg"
-                                className="card-img-top"
-                                alt="Student"
-                                height="300px"
-                            />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">Neha Kapoor</h5>
-                                <p><strong>Admission No:</strong> STU010</p>
-                                <p><strong>Class:</strong> III - A</p>
-                                <button className="btn btn-primary">View Details</button>
-                            </div>
-                        </div>
-                    </div>
+                            )
+                        }
+                    )}
 
                 </div>
             </div>
